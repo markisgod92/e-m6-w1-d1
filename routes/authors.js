1b29089aaa02f216feff3361673dd223841829b3
markisgod92 <markisgod92@gmail.com>
2024-10-15 16:37:08 +0200

    ok obbligatori

--- routes/authors.js
+++ routes/authors.js
@@ -31,6 +31,64 @@ authors.get('/authors', async (req, res) => {
     }
 })
 
+authors.get('/authors/:authorId', async (req, res) => {
+    const {authorId} = req.params
+
+    try {
+        const author = await authorModel.findById(authorId)
+
+        if (!author) {
+            return res
+                .status(404)
+                .send({
+                    message: 'Author at ID not found'
+                })
+        }
+
+        res
+            .status(200)
+            .send({
+                statusCode: 200,
+                author
+            })
+    } catch (e) {
+        res
+            .status(500)
+            .send({
+                message: e.message
+            })
+    }
+})
+
+authors.post('/authors', async (req, res) => {
+    const {nome, cognome, email, birthdate, avatar} = req.body
+
+    const newAuthor = new authorModel({
+        nome: nome,
+        cognome: cognome,
+        email: email,
+        birthdate: birthdate,
+        avatar: avatar
+    })
+
+    try {
+        const savedAuthor = await newAuthor.save()
+
+        res.status(201)
+            .send({
+                statusCode: 201,
+                message: 'Author added successfully',
+                savedAuthor
+            })
+    } catch (e) {
+        res
+            .status(500)
+            .send({
+                message: e.message
+            })
+    }
+})
+
 
 
 
